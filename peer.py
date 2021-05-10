5fa1f9b7219b33d8a0385716d497ef16fbd533a9
Sezin <sezinzeydan51@gmail.com>
2021-05-10 18:03:33 +0300

    flooding completed

--- peer.py
+++ peer.py
@@ -2,52 +2,113 @@ import socket
 import sys
 import threading
 import time
+from datetime import datetime
 
 def connect(conn):
     user =False
     pass_ = False
     authenticated = False
+    floods = {}
     while True:
-        received = conn.recv(1024)
-        if received == ' ':
-            pass
-        else:
-            print(received.decode())
-            #time.sleep(3)
-            if authenticated == False:
+        if authenticated == False:
+            received = conn.recv(1024)
+            if received == ' ':
+                pass
+            else:
                 if "USER bilkentstu" in received.decode():
                     user = True
             
                 if "PASS cs421s2021" in received.decode() and user:
-                    print("HELLLOOO")
+                    #print("HELLLOOO")
                     pass_ = True
                     authenticated = True
                 # print(received.decode())
                     conn.sendall(b'OK\r\n')
-                    print("Basarili")
+                    #print("Basarili")
 
                 if user == False and pass_ == False:
                     conn.sendall(b'INVALID CRED\r\n')
                     print("refused")
-                    #exit(0)
             
+        else:
+            received = conn.recv(17)
+            if received == ' ':
+                pass
+            else:
+                msg = received.decode()
+                if msg not in floods:
+                    #floods += msg
+                    floods[msg] = 1
+                   
+                    thread = threading.Thread(target=send_msg, args=([msg]))
+                    thread.start()
+                    time.sleep(5)
+                    #print("Thread msg is joined")
+                    #thread.join()
+                   
+                else:
+                    floods[msg] += 1
 
+                if len(floods) == 7 * nPeers:
+                    print("flood is ")
+                    print(floods)
+                    print_table(floods)
+                    exit(0)
+        
+        
+           
+                
 
+def print_table(list_f):
+    print("Printing table")
+    print ("{:<14} {:<10} {:<10}".format('Source Node ID','Timestamp','# of messages received'))
+    for key, value in list_f.items():
+        rand = key.find(" ")
+        flood_id = key[rand:rand + 1]
+        timestamp = key[rand+1:rand + 11]
+        print ("{:<14} {:<10} {:<10}".format(flood_id, timestamp, value))
+
+def send_msg(msg):
+    #print("Send message is here")
+    global destinations
+    for port in destinations:
+        #print("Port is " + str(port))
+        s_o = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s_out.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            s_o.connect((ADDRESS, port))
+            #print("connected peers are " + str(s_o.getpeername()))
+            s_out.sendall(msg.encode())
+        except Exception as e:
+            #print(e)
+            pass
+
+            
+            
 def handleClient(s, port_number):
-    print("TCP connection established with peer" + str(port_number))
+    peer_id = port_number - 60000
+    print("TCP connection established with peer " + str(peer_id))
     s.sendall(b'USER bilkentstu\r\n')
     s.sendall(b'PASS cs421s2021\r\n')
-    conn = s.accept()
-    data  = conn.recv(1024)
+    timesup = False
+    data  = s.recv(1024)
     if "OK" in data.decode():
-        while True:
+        print("Authenticated to peer " + str(peer_id))
+        while timesup == False:
             now = datetime.now()
             hour = now.strftime("%H:%M:%S")
             if hour[-2:] == "00":
-                peer_id = port_number - 60000
+                timesup = True
                 counter = 0
+                now = datetime.now()
+                s.sendall(b'FLOD '+ str(PEER_ID).encode() + b' ' + now.strftime("%H:%M:%S").encode() + b'\r\n')
+                counter +=1
                 while counter < 7:
-                    s.sendall()
+                    time.sleep(5)
+                    now = datetime.now()
+                    s.sendall(b'FLOD '+ str(PEER_ID).encode() + b' ' + now.strftime("%H:%M:%S").encode() + b'\r\n')
+                    counter += 1
+               
     else:
         s.shutdown(s.SHUT_RDWR)
         s.close()
@@ -57,22 +118,20 @@ def handleClient(s, port_number):
 arg = sys.argv
 ADDRESS = arg[1]
 PEER_ID = int(arg[2])
-
 PORT_IN = 60000 + PEER_ID
-# PORT_OUT = 50000 + PEER_ID
 USERNAME = "bilkentstu"
 PASSWORD = "cs421s2021"
 
 all_connected = False
 topologyFile = open('topology.txt', 'r')
 lines = topologyFile.readlines()
-print(lines)
+#print(lines)
 
 # extract number of pairs of information
 nPeers = int(lines[0][0])
-print(nPeers)
+#print(nPeers)
 lines = lines[1:]
-print(lines)
+#print(lines)
 destinations = []
 sources = []
 
@@ -94,7 +153,7 @@ s_in.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s_in.bind((ADDRESS, PORT_IN))
 print('listening on', (ADDRESS, PORT_IN))
 
-print(PORT_IN)
+#print(PORT_IN)
 s_in.listen()
 
 while len(threads) < len(destinations):
@@ -116,5 +175,6 @@ thread_conn = threading.Thread(target=connect, args=([conn]))
 thread_conn.start()
 
 for t in threads:
+    print("joining threads")
     t.join()
 thread_conn.join()
